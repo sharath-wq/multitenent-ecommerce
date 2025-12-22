498b51bb6408799c3661a3bf0117739f317cbc13
Sharath P <chandranpsharath@gmail.com>
2025-12-23 00:44:29 +0530

    feat: sign in and sign up flows

--- src/app/(app)/(home)/_components/navbar.tsx
+++ src/app/(app)/(home)/_components/navbar.tsx
@@ -97,13 +97,13 @@ export const Navbar = () => {
           variant={"secondary"}
           className="border-l border-t-0 border-b-0 px-12 h-full rounded-none bg-white hover:bg-green-400 transition-colors text-lg"
         >
-          <Link href={"/sign-in"}>Login</Link>
+          <Link prefetch href={"/sign-in"}>Login</Link>
         </Button>
         <Button
           asChild
           className="border-l border-t-0 border-b-0 px-12 h-full rounded-none bg-black text-white hover:bg-green-400 hover:text-black transition-colors text-lg"
         >
-          <Link href={"/sign-up"}>Start selling</Link>
+          <Link prefetch href={"/sign-up"}>Start selling</Link>
         </Button>
       </div>
 
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.auth.session.queryOptions());
 
   return (
-    <div>
-      Home
-    </div>
+    <pre>
+      {JSON.stringify(data?.user
+        , null, 2)}
+    </pre>
   );
 }
