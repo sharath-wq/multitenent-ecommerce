f0756bb0e51601f542878568ec136c6ade5384ca
Sharath P <chandranpsharath@gmail.com>
2026-01-01 23:21:45 +0530

    feat: product list and product list skelton

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,31 +1,32 @@
-import { Suspense } from "react";
+import { SearchParams } from "nuqs/server";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-import {
-  ProductList,
-  ProductListSkelton,
-} from "@/modules/products/ui/components/product-list";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
 
 interface IProps {
   params: Promise<{
     subcategory: string;
   }>;
+  searchParams: Promise<SearchParams>;
 }
 
-export default async function SubcategoryPage({ params }: IProps) {
+export default async function SubCategoryPage({
+  params,
+  searchParams,
+}: IProps) {
   const { subcategory } = await params;
+  const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
-    trpc.products.getMany.queryOptions({ category: subcategory })
+    trpc.products.getMany.queryOptions({ category: subcategory, ...filters })
   );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<ProductListSkelton />}>
-        <ProductList category={subcategory} />
-      </Suspense>
+      <ProductListView category={subcategory} />
     </HydrationBoundary>
   );
 }
